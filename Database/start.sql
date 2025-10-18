fa3ef622b564d748051c6121e0a17416c9dc69a8
GMMendes01 <g7000mendes@gmail.com>
2025-10-18 10:45:50 -0300

    debug e banco supabase

--- Database/start.sql
+++ Database/start.sql
@@ -1,19 +1,58 @@
-create database IF not exists banco;
-use banco;
+CREATE DATABASE IF NOT EXISTS banco;
+USE banco;
+
 CREATE TABLE cliente (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
-    telefone varchar (20),
-    documento varchar(20),
-    email VARCHAR(30),
-    senha varchar(255)
+    telefone VARCHAR(20),
+    documento VARCHAR(20),
+    email VARCHAR(50),
+    senha VARCHAR(255),
+    role ENUM('cliente', 'admin') DEFAULT 'cliente'
 );
-CREATE TABLE admin (
+
+CREATE TABLE veiculo (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(50) NOT NULL,
-    email VARCHAR(30),
-    senha varchar(255)
+    nome VARCHAR(100), 
+    marca VARCHAR(50),
+    placa VARCHAR(20) NOT NULL UNIQUE,
+    capacidade INT,
+    ano YEAR,
+    status ENUM('disponivel', 'em uso', 'manutencao') DEFAULT 'disponivel'
 );
+
+CREATE TABLE pedido (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id INT NOT NULL,
+    veiculo_id INT DEFAULT NULL,
+    data_pedido DATE NOT NULL,
+    entregue_em DATETIME DEFAULT NULL, 
+    criado_em DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (cliente_id) REFERENCES cliente(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (veiculo_id) REFERENCES veiculo(id) 
+);
+
+
+CREATE TABLE pedido_status (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    pedido_id INT NOT NULL,
+    status ENUM('pendente', 'em transporte', 'entregue', 'cancelado'),
+    data_alteracao DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (pedido_id) REFERENCES pedido(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE endereco (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id INT NOT NULL,
+    logradouro VARCHAR(100),
+    numero VARCHAR(10),
+    bairro VARCHAR(50),
+    cidade VARCHAR(50),
+    cep VARCHAR(10),
+    FOREIGN KEY (cliente_id) REFERENCES cliente(id) ON DELETE CASCADE
+);
+
 
 
 
