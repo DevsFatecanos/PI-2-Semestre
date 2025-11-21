5acc69cc6f08c84135701fb0f27feb650c393939
Ryan <silvallucena02@gmail.com>
2025-11-21 14:04:24 -0300

    add valor class  veiculo

--- Model/Veiculo.php
+++ Model/Veiculo.php
@@ -3,13 +3,15 @@ class Veiculo {
     public $id_veiculo;
     public $modelo;
     public $placa;
+    public $valor_por_km;
     public $status;
 
-    public function __construct($id_veiculo, $modelo, $placa, $status) {
+    public function __construct($id_veiculo, $modelo, $placa,$valor_por_km, $status) {
         $this->id_veiculo = $id_veiculo;
         $this->modelo = $modelo;
         $this->placa = $placa;
         $this->status = $status;
+        $this->valor_por_km = $valor_por_km;
     }
 }
 ?>
